86aff41469fa017097336491aae8b02dbb85687c
codebull <43239976+srkrep@users.noreply.github.com>
2023-07-15 18:28:59 -0400

    First Commit
    Liva a new project

---
+++ liva--project/liva/src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <h1>Welcome To Liva</h1>
+      </div>
+      <div>
+        <h2>The smart way to share home</h2>
+      </div>
+    </main>
+  )
+}
